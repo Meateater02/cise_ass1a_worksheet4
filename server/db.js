fa9b9afdcc59db8ca133f784f4f2945ecc7c8946
Meateater02 <dqh.phan@gmail.com>
2022-08-16 21:41:02 +1200

    set up back end

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const config = require('server');
+const config = require('config');
 // const db = config.get('mongoURI');
 
 const dotenv = require("dotenv");
